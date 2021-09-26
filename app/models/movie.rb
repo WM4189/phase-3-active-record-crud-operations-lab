aef921e61cb5e7607b861fae946004247d7c323a
William Mosto <will.mosto@gmail.com>
2021-09-26 19:00:01 -0400

    commit tree d5087337fead119ebf1e51d594fd5a08e8537546

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,52 @@
 class Movie < ActiveRecord::Base
+#CREATE
+    def self.create_with_title(title)
+        self.create(title: title)
+    end
+
+    def self.first_movie
+        self.find(1)
+    end
+
+    def self.last_movie
+        self.last
+    end
+
+    def self.movie_count
+        self.count
+    end
+
+    def self.find_movie_with_id(id)
+        self.find(id)
+    end
+
+    def self.find_movie_with_attributes(att)
+        self.find_by(att)
+    end
+
+    def self.find_movies_after_2002
+        self.where("release_date > 2002")
+    end
+
+#UPDATE
+    def update_with_attributes(attributes)
+        self.attributes = attributes
+        self.save
+    end
+
+    def self.update_all_titles(title)
+        self.update(:title => title)
+    end
+    
+#DELETE
+    def self.delete_by_id(id)
+        self.find(id).destroy
+    end
+
+    def self.delete_all_movies
+        self.destroy_all
+    end
+
+
 
 end
